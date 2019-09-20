123bef836dae231c9ad3e66c7efdde31a30ad987
muriloconceicao <daconceicaomurilo@gmail.com>
2019-09-20 00:09:15 -0300

    Added FutureBuilder.

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,65 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
-
 const request = "https://api.hgbrasil.com/finance?format=json&key=3ed3d24a";
 
 void main() async {
-
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)["results"]["currencies"]["USD"]);
+  print(await getData());
 
   runApp(MaterialApp(
-    home: Container(
-    ),
+    home: Home(),
   ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Conversor de Moedas"),
+        backgroundColor: Colors.amberAccent,
+        centerTitle: true,
+      ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (contenxt, snapshop) {
+          switch (snapshop.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                  child: Text(
+                "Carregando dados.",
+                style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                textAlign: TextAlign.center,
+              ));
+            default:
+              if (snapshop.hasError) {
+                return Center(
+                  child: Text(
+                    "Erro ao carregar dados.",
+                    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else {
+                return Container(
+                  color: Colors.lightBlueAccent,
+                );
+              }
+          }
+        },
+      ),
+    );
+  }
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
 }
