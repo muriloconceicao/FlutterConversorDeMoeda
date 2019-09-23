bb63bde694e6e0df15e817619b8befb37757de26
muriloconceicao <daconceicaomurilo@gmail.com>
2019-09-23 20:40:05 -0300

    Final commit.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
@@ -6,17 +8,15 @@ import 'dart:convert';
 const request = "https://api.hgbrasil.com/finance?format=json&key=3ed3d24a";
 
 void main() async {
-  print(await getData());
 
   runApp(MaterialApp(
     home: Home(),
     theme: ThemeData(
-      hintColor: Colors.lightGreen,
-      primaryColor: Colors.black,
-      inputDecorationTheme: InputDecorationTheme(
-        enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.lightGreen))
-      )
-    ),
+        hintColor: Colors.black38,
+        primaryColor: Colors.black,
+        inputDecorationTheme: InputDecorationTheme(
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black12)))),
   ));
 }
 
@@ -26,6 +26,12 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  double real;
   double dolar;
   double euro;
 
@@ -35,7 +41,7 @@ class _HomeState extends State<Home> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("Conversor de Moedas"),
-        backgroundColor: Colors.lightGreen,
+        backgroundColor: Colors.black,
         centerTitle: true,
       ),
       body: FutureBuilder<Map>(
@@ -47,7 +53,7 @@ class _HomeState extends State<Home> {
               return Center(
                   child: Text(
                 "Carregando dados.",
-                style: TextStyle(color: Colors.lightGreen, fontSize: 25.0),
+                style: TextStyle(color: Colors.black, fontSize: 25.0),
                 textAlign: TextAlign.center,
               ));
             default:
@@ -55,7 +61,7 @@ class _HomeState extends State<Home> {
                 return Center(
                   child: Text(
                     "Erro ao carregar dados.",
-                    style: TextStyle(color: Colors.lightGreen, fontSize: 25.0),
+                    style: TextStyle(color: Colors.black, fontSize: 25.0),
                     textAlign: TextAlign.center,
                   ),
                 );
@@ -67,17 +73,16 @@ class _HomeState extends State<Home> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: <Widget>[
-                      Icon(Icons.monetization_on, size: 150.0, color: Colors.lightGreen),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: "Reais",
-                          labelStyle: TextStyle(color: Colors.lightGreen),
-                          border: OutlineInputBorder(),
-                          prefixText: "R\$: "
-                        ),
-                        style: TextStyle(
-                          color: Colors.lightGreen, fontSize: 25.0
-                        ),
+                      Icon(Icons.monetization_on,
+                          size: 150.0, color: Colors.black),
+                      buildTextField("Reais", "R\$: ", realController, _realChange),
+                      Padding(
+                        padding: EdgeInsets.only(top: 8.0),
+                        child: buildTextField("Dólares", "US\$: ", dolarController, _dolarChange),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 8.0),
+                        child: buildTextField("Euros", "€: ", euroController, _euroChanged),
                       )
                     ],
                   ),
@@ -88,9 +93,42 @@ class _HomeState extends State<Home> {
       ),
     );
   }
+
+  void _realChange(String text) {
+    double real = double.parse(text);
+    dolarController.text = (real/dolar).toStringAsFixed(2);
+    euroController.text = (real/euro).toStringAsFixed(2);
+  }
+
+  void _dolarChange(String text) {
+    double dolar  = double.parse(text);
+    realController.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
+  }
+
+  void _euroChanged(String text) {
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+  }
+
 }
 
 Future<Map> getData() async {
   http.Response response = await http.get(request);
   return json.decode(response.body);
+}
+
+Widget buildTextField(String label, String prefix, TextEditingController textEditingController, Function function) {
+  return TextField(
+    controller: textEditingController,
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.black),
+        border: OutlineInputBorder(),
+        prefixText: prefix),
+    style: TextStyle(color: Colors.black, fontSize: 25.0),
+    onChanged: function,
+    keyboardType: TextInputType.number,
+  );
 }
